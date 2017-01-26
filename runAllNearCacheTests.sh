546aeed138c145b0eb2ea88abb319c3bd464851b
ihsan demir <ihsan@hazelcast.com>
2017-01-26 15:33:08 +0300

    update

--- runAllNearCacheTests.sh
+++ runAllNearCacheTests.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-START_DIR=`pwd
+START_DIR=`pwd`
 
-CPP_CLIENT_HOME=/home/ihsan/work/src/hazelcast/hazelcast-client
+START_TIME=`date +%Y%m%d%H%M%S`
+
+CPP_CLIENT_HOME=/home/ihsan/work/src/hazelcast-cpp-client
 CPP_CLIENT_BUILD_DIR=${CPP_CLIENT_HOME}/buildSTATIC64Release
 CPP_EXE_PATH=${CPP_CLIENT_BUILD_DIR}/hazelcast/test/performance/near-cache/nearCachePerformance
 JAVA_CLIENT_HOME=/home/ihsan/work/src/hazelcast/hazelcast-client
 
 cd ${CPP_CLIENT_HOME}
 
-${CPP_CLIENT_HOME}/runAllNearCacheCppTests.sh ${CPP_EXE_PATH} 10.212.1.111 2>&1 | tee cppNearCachePerfTestRunLog`date +%Y%m%d%H%M%S`.txt
+${CPP_CLIENT_HOME}/runAllNearCacheCppTests.sh ${CPP_EXE_PATH} 10.212.1.111 2>&1 | tee cppNearCachePerfTestRunLog${START_TIME}.txt
 
 cd ${JAVA_CLIENT_HOME}
 
-${CPP_CLIENT_HOME}/runAllNearCacheJavaTests.sh 10.212.1.111 2>&1 | tee javaNearCachePerfTestRunLog`date +%Y%m%d%H%M%S`.txt
+${CPP_CLIENT_HOME}/runAllNearCacheJavaTests.sh 10.212.1.111 2>&1 | tee javaNearCachePerfTestRunLog${START_TIME}.txt
 
 cd ${START_DIR}
 
